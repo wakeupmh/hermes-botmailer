50992e321d36f83d8cbdbf1d3443215d9e274d93
wakeupmh <wakeupmh@gmail.com>
2020-06-07 01:38:30 -0300

    refactor(project): add missing imports

--- api/send.js
+++ api/send.js
@@ -1,7 +1,7 @@
 import Bluebird from 'bluebird'
 import { sendMail, mailSchema } from '../mail'
 import { Logger } from '../infrastructure'
-import { validateSchema } from '../common'
+import { validateSchema, handleSuccessResponse, handleErrorResponse } from '../common'
 
 export default (req, res) => {
   return Bluebird.resolve(req.body)
